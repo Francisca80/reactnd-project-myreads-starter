4a273dbdaec874d2d743869620605524d5cc3d45
Maki <francisca@MacBook-Pro-van-Maki.local>
2019-02-04 19:21:16 +0100

    commit with updates on search and added oneBook for single books

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import Search from "./Search"
 import BooksPage from "./BooksPage"
 
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []  /** The list of books */
+  };
+ 
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
+
+
+  /**
+   * Moving books to a shelf and update the shelf
+   */
+
+  moveShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+
+    BooksAPI.getAll().then(books => {
+      this.setState({ books: books });
+    });
+  };
 
   render() {
     return (
@@ -27,13 +41,17 @@ class BooksApp extends React.Component {
 
         <Route path="/search"
           render={() => (
-          <Search/>
-          )}/>
+          
+              <Search moveShelf={this.moveShelf} books={this.state.books} />
+            )}
+          />
 
           {/* Render the home page */}
         <Route exact path='/' render={() => (
-          <BooksPage/>
-        )}/>
+             <BooksPage books={this.state.books} moveShelf={this.moveShelf} />
+             )}
+           />
+        
           
            
       </div>
--- src/Search.js
+++ src/Search.js
@@ -1,35 +1,80 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import oneBook from "./oneBook";
+import * as BooksAPI from "./BooksAPI";
 
-
-
+/**
+ * The search page component. 
+ * Shows the books in the query, if it is empty then nothing shows.
+ */
 
 class Search extends Component{
-    render(){
-        return(
-
-<div className="search-books">
+ 
+    state = {
+        query: "",
+        searchedBooks: []
+      };
+    
+      updateQuery = query => {
+        this.setState({
+          query: query
+        });
+        this.updateSearchedBooks(query);
+      };
+    
+      updateSearchedBooks = query => {
+        if (query) {
+          BooksAPI.search(query).then(searchedBooks => {
+            if (searchedBooks.error) {
+              this.setState({ searchedBooks: [] });
+            } else {
+              this.setState({ searchedBooks: searchedBooks });
+            }
+          });
+        } else {
+          this.setState({ searchedBooks: [] });
+        }
+      };
+      render() {
+        return (
+          <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <Link to="/" className="close-search">
+                Close
+              </Link>
+    
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
+                <input
+                  type="text"
+                  placeholder="Search by title or author"
+                  value={this.state.query}
+                  onChange={event => this.updateQuery(event.target.value)}
+                />
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                {this.state.searchedBooks.map(searchedBook => {
+                  let shelf = "none";
+    
+                  this.props.books.map(
+                    book =>
+                      book.id === searchedBook.id ? (shelf = book.shelf) : ""
+                  );
+                  return (
+                    <li key={searchedBook.id}>
+                      <oneBook
+                        book={searchedBook}
+                        moveShelf={this.props.moveShelf}
+                        currentShelf={shelf}
+                      />
+                    </li>
+                  );
+                })}
+              </ol>
             </div>
-            </div>
-        )}
-            }
-        
-        
-        
-export default Search;
+          </div>
+        );
+      }
+    }
+export default Search    
